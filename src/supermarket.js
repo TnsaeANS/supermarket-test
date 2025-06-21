1c992b754200d3070bb125b33078aa19d213621f
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2025-06-21 23:18:28 +0300

    Implement cart functionality in Supermarket component

--- src/supermarket.js
+++ src/supermarket.js
@@ -1,8 +1,67 @@
 import './App.css';
-
-
+import { useState } from 'react';
 
 function Supermarket() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart(prevCart => {
+      const existingItem = prevCart.find(cartItem => cartItem.id === item.id);
+      
+      if (existingItem) {
+        return prevCart.map(cartItem =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        return [...prevCart, { ...item, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeFromCart = (itemId) => {
+    setCart(prevCart => prevCart.filter(item => item.id !== itemId));
+  };
+
+  const updateQuantity = (itemId, newQuantity) => {
+    if (newQuantity < 1) {
+      removeFromCart(itemId);
+      return;
+    }
+
+    setCart(prevCart =>
+      prevCart.map(item =>
+        item.id === itemId ? { ...item, quantity: newQuantity } : item
+      )
+    );
+  };
+
+  const incrementQuantity = (itemId) => {
+    updateQuantity(itemId, 
+      (cart.find(item => item.id === itemId)?.quantity || 0) + 1
+    );
+  };
+
+  const decrementQuantity = (itemId) => {
+    const currentItem = cart.find(item => item.id === itemId);
+    if (currentItem) {
+      updateQuantity(itemId, currentItem.quantity - 1);
+    }
+  };
+
+  const getTotalItems = () => {
+    return cart.reduce((total, item) => total + item.quantity, 0);
+  };
+
+  const getTotalPrice = () => {
+    return cart.reduce((total, item) => total + (item.price * item.quantity), 0);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
   return (
     <div className="App">
     </div>
---
+++ src/supermarket.test.js
@@ -0,0 +1,2 @@
+import Supermarket from "./supermarket";
+
