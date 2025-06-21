38dd51c35777d142dee70b6791ab21139b2123e0
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2025-06-21 20:04:14 +0300

    initial commit: remove App component and introduce Supermarket component

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Supermarket from './supermarket';
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ src/supermarket.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+
+function Supermarket() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default Supermarket;
